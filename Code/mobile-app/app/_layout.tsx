7783b385edf53c64d090abba047654ae6ade549c
OsbournD <douglas.osbourn@students.plymouth.ac.uk>
2025-11-24 17:18:55 +0000

    Set up Expo Dev Build for Android app. - Set up android version of app for push notification testing via Expo.

--- Code/mobile-app/app/_layout.tsx
+++ Code/mobile-app/app/_layout.tsx
@@ -4,6 +4,16 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+    }),
+});
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,6 +21,29 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+      const registerForPush = async () => {
+          const { status: existingStatus } = await Notifications.getPermissionsAsync(); // check current permission status.
+          let finalStatus = existingStatus;
+
+          if (existingStatus !== 'granted') {
+              const { status } = await Notifications.requestPermissionsAsync(); // if not granted, request again.
+              finalStatus = status;
+          }
+
+          if (finalStatus !== 'granted') {
+              console.log("Notification permissions not granted."); // if still not granted, stop.
+              return;
+          }
+
+          const tokenData = await Notifications.getExpoPushTokenAsync(); // get expo push token.
+          console.log("Expo push token: ", tokenData.data);
+      }
+
+      registerForPush();
+
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
