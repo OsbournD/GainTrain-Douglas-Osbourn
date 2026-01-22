a92174ea1be011c6d11b2e362bafbcd956e343a8
OsbournD <douglas.osbourn@students.plymouth.ac.uk>
2026-01-22 17:09:59 +0000

    Workout logger screen, Inputs are half done, UI layout is better and hide/show top section button works. Dropdown also works better.

--- Code/mobile-app/app/workout/logger.tsx
+++ Code/mobile-app/app/workout/logger.tsx
@@ -9,28 +9,39 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function workoutLogger() {
 
+    const [showTemplateDropdown, setShowTemplateDropdown] = useState(false);
+    const [templateList, setTemplateList] = useState([
+        "Push Day",
+        "Pull Day",
+        "Legs",
+        "Full Body",
+    ]);
+    const [selectedTemplate, setSelectedTemplate] = useState("");
+
+    const [showSetup, setShowSetup] = useState(true);
+
     const router = useRouter();
-        const [checkingLogin, setCheckingLogin] = useState(true);
+    const [checkingLogin, setCheckingLogin] = useState(true);
 
-        useEffect(() => {
-            const checkLogin = async () => {
-                const storedUser = await AsyncStorage.getItem('loggedInUser');
-                if (!storedUser) {
-                    router.replace('/(auth)/login');
-                    return;
-                }
-                setCheckingLogin(false);
-            };
-            checkLogin();
-        }, []);
+    useEffect(() => {
+        const checkLogin = async () => {
+            const storedUser = await AsyncStorage.getItem('loggedInUser');
+            if (!storedUser) {
+                router.replace('/(auth)/login');
+                return;
+            }
+            setCheckingLogin(false);
+        };
+        checkLogin();
+    }, []);
 
-        if (checkingLogin) {
-            return (
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <ActivityIndicator size="large" />
-                </View>
-            );
-        }
+    if (checkingLogin) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" />
+            </View>
+        );
+    }
 
     const backToDashboardClicked = async () => {
         try {
@@ -52,7 +63,7 @@ export default function workoutLogger() {
 
     return(
 
-        <View style={ styles.appBackground }>
+        <View style={ [styles.appBackground, { position: 'relative' }] }>
 
             <ThemedView style={styles.headerContainer}>
 
@@ -70,16 +81,47 @@ export default function workoutLogger() {
 
             <View style = { styles.spacer }/>
 
+            <View>
+
+                {showSetup && (
+
+                    <View style = { styles.card }>
+
+                        <ThemedText style = { styles.headingText }>Session Setup</ThemedText>
+                        <TextInput style = { styles.input } placeholder = "Session Name" />
+                        <TextInput style = { styles.input } placeholder = "Date & Time" />
+                        <TextInput style = { styles.notesInput } placeholder = "Session Notes (optional)" multiline />
+
+                        <View style = {{ marginTop: 10 }}>
+
+                            <TouchableOpacity
+                                style = { styles.input }
+                                onPress = {() => setShowTemplateDropdown(!showTemplateDropdown)}
+                            >
+                                <Text style = {{ color: selectedTemplate ? 'black' : '#24C3FF' }}>
+                                    { selectedTemplate || "Choose Template..." }
+                                </Text>
+                            </TouchableOpacity>
+
+                        </View>
+                    </View>
+
+                )}
+
+                <TouchableOpacity style = { styles.hideButton }
+                    onPress={() => {
+                        setShowSetup(!showSetup);
+                        setShowTemplateDropdown(false);
+                    }}>
+                    <Text style = { styles.collapseText }>
+                        {showSetup ? '- Hide Session Setup' : '+ Show Session Setup'}
+                    </Text>
+                </TouchableOpacity>
+
+
+            </View>
+
             <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
-
-                <View style = { styles.card }>
-
-                    <ThemedText style = { styles.headingText }>Session Setup</ThemedText>
-                    <TextInput style = { styles.input } placeholder = "Session Name" />
-                    <TextInput style = { styles.input } placeholder = "Date & Time" />
-                    <TextInput style = { styles.notesInput } placeholder = "Session Notes (optional)" multiline />
-                    <TextInput style = { styles.input } placeholder = "Template (optional)" />
-                </View>
 
                 <View style = { styles.card }>
 
@@ -109,28 +151,97 @@ export default function workoutLogger() {
 
                     </View>
 
+                    <View style = { styles.exerciseCard }>
+
+                        <Text style = { styles.exerciseTitle }>Bench Press Again!</Text>
+                        <Text style = { styles.setText }>60kg x 8 - 3sec negative - RPE 7</Text>
+                        <Text style = { styles.setText }>65kg x 6 - Wrist wraps, Pause - RPE 8</Text>
+                        <Text style = { styles.setText }>65kg x 6 - Wrist wraps, Pause - RPE 8</Text>
+
+                        <View style = { styles.buttonRow }>
+
+                            <TouchableOpacity style = { styles.addButton }>
+                                <Text style = { styles.exerciseButtonText }>+ Add Set</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style = { styles.editButton }>
+                                <Text style = { styles.exerciseButtonText }>Edit</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style = { styles.removeButton }>
+                                <Text style = { styles.exerciseButtonText }>Remove</Text>
+                            </TouchableOpacity>
+
+                        </View>
+
+                    </View>
+
+                    <View style = { styles.exerciseCard }>
+
+                        <Text style = { styles.exerciseTitle }>Bench Press Again AGAIN!</Text>
+                        <Text style = { styles.setText }>60kg x 8 - 3sec negative - RPE 7</Text>
+                        <Text style = { styles.setText }>65kg x 6 - Wrist wraps, Pause - RPE 8</Text>
+                        <Text style = { styles.setText }>65kg x 6 - Wrist wraps, Pause - RPE 8</Text>
+                        <Text style = { styles.setText }>60kg x 8 - 3sec negative - RPE 7</Text>
+
+                        <View style = { styles.buttonRow }>
+
+                            <TouchableOpacity style = { styles.addButton }>
+                                <Text style = { styles.exerciseButtonText }>+ Add Set</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style = { styles.editButton }>
+                                <Text style = { styles.exerciseButtonText }>Edit</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style = { styles.removeButton }>
+                                <Text style = { styles.exerciseButtonText }>Remove</Text>
+                            </TouchableOpacity>
+
+                        </View>
+
+                    </View>
+
                     <TouchableOpacity style = { styles.addExerciseButton }>
                         <Text style = { styles.buttonText }>+ Add Exercise</Text>
                     </TouchableOpacity>
 
                 </View>
 
-                <View style = { styles.buttonRow }>
-                    <TouchableOpacity style = { styles.saveButton }>
-                        <Text style = { styles.buttonText }>Save Session</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style = { styles.laterButton }>
-                        <Text style = { styles.buttonText }>Finish Later</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style = { styles.deleteButton }>
-                        <Text style = { styles.buttonText }>Delete Session</Text>
-                    </TouchableOpacity>
-                </View>
-
             </ScrollView>
 
+            <View style = { styles.fixedButtonRow }>
+                <TouchableOpacity style = { styles.saveButton }>
+                    <Text style = { styles.buttonText }>Save Session</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = { styles.laterButton }>
+                    <Text style = { styles.buttonText }>Finish Later</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = { styles.deleteButton }>
+                    <Text style = { styles.buttonText }>Delete Session</Text>
+                </TouchableOpacity>
+            </View>
+
+            {showTemplateDropdown && (
+                <View style = { styles.dropdownMenu }>
+                    {templateList.map( (template, index) => (
+                        <TouchableOpacity
+                            key = { index }
+                            style = { styles.dropdownItem }
+                            onPress = { () => {
+                                setSelectedTemplate(template);
+                                setShowTemplateDropdown(false);
+                            }}
+                        >
+
+                            <Text style = { styles.dropdownItemText }>{template}</Text>
+                        </TouchableOpacity>
+                    ))}
+
+                </View>
+            )}
 
         </View>
 
@@ -351,10 +462,71 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         marginTop: 8,
     },
+    fixedButtonRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingVertical: 20,
+        backgroundColor: 'white',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 8,
+    },
     buttonText: {
         fontSize: 18,
         fontWeight: 'bold',
         color: 'white',
         textAlign: 'center',
+    },
+    dropdownMenu: {
+        position: 'absolute',
+        top: 390,
+        backgroundColor: 'white',
+        alignSelf: 'flex-start',
+        marginHorizontal: 20,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#D9D9D9',
+        marginTop: 4,
+        overflow: 'hidden',
+        padding: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 4,
+        zIndex: 100,
+    },
+    dropdownItem: {
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        borderBottomWidth: 1,
+        borderBottomColor: 'black',
+    },
+    dropdownItemText: {
+        fontSize: 14,
+        color: 'black',
+    },
+    collapseText: {
+        textAlign: 'center',
+        color: '#24C3FF',
+        fontWeight: 'bold',
+        fontSize: 14,
+    },
+    hideButton: {
+        backgroundColor: 'white',
+        padding: 2,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 10,
+        marginHorizontal: 20,
+        marginVertical: 14,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 6,
     },
 });
