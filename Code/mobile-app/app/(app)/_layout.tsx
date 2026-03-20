df5674538a5c2aeda007d2a4a08a206aa5be03ed
OsbournD <douglas.osbourn@students.plymouth.ac.uk>
2026-03-20 11:40:25 +0000

    Removed profile screen and tab.

--- Code/mobile-app/app/(app)/_layout.tsx
+++ Code/mobile-app/app/(app)/_layout.tsx
@@ -34,7 +34,6 @@ export default function AppLayout() {
         }}>
 
             <Tabs.Screen name = "dashboard" options = {{ title: "Dashboard"}} />
-            <Tabs.Screen name = "profile" options = {{ title: "Profile"}} />
             <Tabs.Screen name = "community" options = {{ title: "Community"}} />
 
         </Tabs>
